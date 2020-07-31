f176e948c353e45ea2d63dd2f200ca16d0f683b7
Aman Jain <amanj120@gmail.com>
2020-07-31 16:17:10 -0400

    Proof of concept for Play Asset Delivery publishing in Godot game engine.

--- platform/android/export/gradle_export_util.h
+++ platform/android/export/gradle_export_util.h
@@ -37,6 +37,18 @@
 #include "core/os/os.h"
 #include "editor/editor_export.h"
 
+enum AssetPackDeliveryMode {
+	INSTALL_TIME,
+	FAST_FOLLOW,
+	ON_DEMAND,
+};
+
+typedef struct _AssetPackInfo {
+	String file_path;
+	AssetPackDeliveryMode delivery_mode;
+	String name;
+} AssetPackInfo;
+
 const String godot_project_name_xml_string = R"(<?xml version="1.0" encoding="utf-8"?>
 <!--WARNING: THIS FILE WILL BE OVERWRITTEN AT BUILD TIME-->
 <resources>
@@ -242,4 +254,231 @@ String _get_application_tag(const Ref<EditorExportPreset> &p_preset, const Strin
 	return manifest_application_text;
 }
 
+/*
+ * This method writes to the build.gradle and settings.gradle files in the root Gradle project.
+ */
+Error _update_root_project_with_asset_pack_info(Vector<AssetPackInfo> &p_asset_pack_info) {
+	//TODO: completely overwrite the asset pack info in root directory settings.gradle and build.gradle file
+	String settings_gradle_string = "//ASSET_PACK_INFO_START !!!DO NOT EDIT THIS LINE!!!\ninclude ':app'\n";
+	String build_gradle_string = "//ASSET_PACK_INFO_START !!!DO NOT EDIT THIS LINE!!!\nassetPacks = [";
+	for (int i = 0; i < p_asset_pack_info.size(); i++) {
+		String name = p_asset_pack_info.get(i).name;
+		settings_gradle_string += vformat("include ':%s'\n", name);
+		build_gradle_string += vformat("\":%s\"", name);
+		if (i < p_asset_pack_info.size() - 1) {
+			build_gradle_string += ", ";
+		}
+	}
+	build_gradle_string += "]\n";
+
+	print_line(build_gradle_string); //TODO: delete unnecessary debugging print statements
+	print_line(settings_gradle_string); //TODO: delete unnecessary debugging print statements
+
+	Error err;
+	String build_gradle_file_string = FileAccess::get_file_as_string("res://android/build/build.gradle", &err);
+	String settings_gradle_file_string = FileAccess::get_file_as_string("res://android/build/settings.gradle", &err);
+	String begin_tag = "//ASSET_PACK_INFO_START !!!DO NOT EDIT THIS LINE!!!";
+	String end_tag = "//ASSET_PACK_INFO_END !!!DO NOT EDIT THIS LINE!!!";
+	int b_start = build_gradle_file_string.find(begin_tag);
+	int b_end = build_gradle_file_string.find(end_tag);
+	int b_len = build_gradle_file_string.length();
+	int s_start = settings_gradle_file_string.find(begin_tag);
+	int s_end = settings_gradle_file_string.find(end_tag);
+	int s_len = settings_gradle_file_string.length();
+	String b_write = build_gradle_file_string.substr(0, b_start) +
+					 build_gradle_string + build_gradle_file_string.substr(b_end, b_len - b_end);
+	String s_write = settings_gradle_file_string.substr(0, s_start) +
+					 settings_gradle_string + settings_gradle_file_string.substr(s_end, s_len - s_end);
+
+	store_string_at_path("res://android/build/build.gradle", b_write);
+	store_string_at_path("res://android/build/settings.gradle", s_write);
+
+	return OK;
+}
+
+/*
+ * This method recursively searches the res:// directory for any .pck files that may not have been
+ * included in the Asset Pack Gradle module.
+ */
+Error _handle_pack_files(const String &path, const AssetPackInfo &packInfo) {
+	print_line("path: res://" + path); //TODO: delete unnecessary debugging print statements
+	DirAccess *da = DirAccess::open("res://" + path);
+	if (!da) {
+		return ERR_CANT_OPEN;
+	}
+	da->list_dir_begin();
+	while (true) {
+		String file = da->get_next();
+		print_line(file); //TODO: delete unnecessary debugging print statements
+		if (file == "") {
+			break;
+		}
+		if (file == "." || file == "..") {
+			continue;
+		}
+		if (file.ends_with(".pck") || file.ends_with(".zip")) {
+			String dest_base_path = path.replace_first(packInfo.file_path, "");
+			String dest = "res://android/build/" + packInfo.name + "/src/main/assets/" + dest_base_path + file;
+			da->make_dir_recursive(dest.get_base_dir());
+			print_line("destination: " + dest); //TODO: delete unnecessary debugging print statements
+			da->copy(path + file, dest);
+		} else if (da->dir_exists(file)) {
+			Error err = _handle_pack_files(path + file + "/", packInfo);
+			if (err != OK) {
+				da->list_dir_end();
+				return err;
+			}
+		}
+	}
+	da->list_dir_end();
+	return OK;
+}
+
+/*
+ * This method copies the compiled assets from the `res://android/build/assets/` directory to the
+ * appropriate folder in the Asset Pack module
+ */
+Error _copy_asset_directory_files(const AssetPackInfo &packInfo) {
+	DirAccess *da = DirAccess::create(DirAccess::ACCESS_RESOURCES);
+	String asset_path_from = "res://android/build/assets/" + packInfo.file_path;
+	String asset_path_to = "res://android/build/" + packInfo.name + "/src/main/assets/";
+	print_line(asset_path_to); //TODO: delete unnecessary debugging print statements
+	print_line(asset_path_from); //TODO: delete unnecessary debugging print statements
+	da->make_dir_recursive(asset_path_to);
+	//	Error err = da->copy(asset_path_from, asset_path_to);
+	Error err = da->rename(asset_path_from, asset_path_to);
+	if (err != OK) {
+		print_line("could not rename"); //TODO: delete unnecessary debugging print statements
+		//		return err;
+	}
+	print_line("dealing with pack files now"); //TODO: delete unnecessary debugging print statements
+	err = _handle_pack_files(packInfo.file_path, packInfo);
+
+	return err;
+}
+
+/*
+ * This method creates the Gradle Asset Pack module by writing the module's build.gradle file and
+ * then calling `_copy_asset_directory_files()` which copies the assets from the appropriate
+ * directories into the gradle module.
+ */
+Error _create_asset_pack_folder(const AssetPackInfo &packInfo) {
+	String packName = packInfo.name;
+	String deliveryType = "install-time"; //default
+	if (packInfo.delivery_mode == INSTALL_TIME) {
+		deliveryType = "install-time";
+	}
+	if (packInfo.delivery_mode == FAST_FOLLOW) {
+		deliveryType = "fast-follow";
+	}
+	if (packInfo.delivery_mode == ON_DEMAND) {
+		deliveryType = "on-demand";
+	}
+	String build_gradle_string = vformat(
+			"apply plugin: 'com.android.asset-pack'\n\n"
+			"assetPack {\n"
+			"    packName = \"%s\"\n"
+			"    dynamicDelivery {\n"
+			"        deliveryType = \"%s\"\n"
+			"    }\n"
+			"}\n",
+			packName, deliveryType);
+	String build_gradle_path = vformat("res://android/build/%s/build.gradle", packName);
+	store_string_at_path(build_gradle_path, build_gradle_string);
+	Error err = _copy_asset_directory_files(packInfo);
+	return err;
+}
+
+Error _read_asset_pack_config_file(const String config_filepath, Vector<AssetPackInfo> &r_asset_pack_info) {
+	FileAccess *f = FileAccess::open(config_filepath, FileAccess::READ);
+	if (f == NULL) {
+		return ERR_DOES_NOT_EXIST;
+	}
+	while (!f->eof_reached()) {
+		/*
+		 * This while loop reads the file in chunks of three lines at a time, and it assumes that
+		 * the file is formatted correctly. As you can see, there is no error checking here.
+		 * The format of the asset pack config file must be very strictly enforced, or else this
+		 * method will break.
+		 *
+		 * The first line is the path, the second line is the delivery mode, and the third line is
+		 * the name of the asset pack.
+		 */
+		AssetPackInfo temp_info;
+
+		String path = f->get_line().strip_edges();
+		int mode = f->get_line().strip_edges().get(0) - 48; //48 = Ascii 0
+		String asset_pack_name = f->get_line().strip_edges();
+
+		temp_info.file_path = path;
+		temp_info.delivery_mode = (AssetPackDeliveryMode)mode;
+		temp_info.name = asset_pack_name;
+
+		r_asset_pack_info.push_back(temp_info);
+	}
+	return OK;
+}
+
+Error _handle_asset_packs(const String config_filepath) {
+	//TODO: check for errors
+	Vector<AssetPackInfo> asset_pack_info;
+	/*
+	 * _read_asset_pack_config_file populates the asset_pack_info vector by parsing the contents of
+	 * the asset pack config file.
+	 */
+	_read_asset_pack_config_file(config_filepath, asset_pack_info);
+	for (int i = 0; i < asset_pack_info.size(); i++) {
+		/*
+		 * _create_asset_pack_folder creates the gradle module for each asset pack
+		 */
+		_create_asset_pack_folder(asset_pack_info.get(i));
+	}
+	/*
+	 * _update_root_project_with_asset_pack_info updates the root gradle project's buid.gradle and
+	 * settings.gradle files
+	 */
+	_update_root_project_with_asset_pack_info(asset_pack_info);
+	return OK;
+}
+
+/*
+ * This method deletes everything that is not part of the base Gradle project
+ */
+Error delete_asset_folders() {
+	print_line("deleting asset folders"); //TODO: delete unnecessary debugging print statements
+	DirAccessRef da = DirAccess::open("res://android/build");
+	if (!da) {
+		return ERR_CANT_OPEN;
+	}
+	da->list_dir_begin();
+
+	String keep[] = { ".", "..", "res", "AndroidManifest.xml", "config.gradle",
+		"libs", "gradle", "gradlew", ".gdignore", "build.gradle",
+		"gradle.properties", "gradlew.bat", "settings.gradle", "src" };
+
+	//TODO: Use a better data structure, generate this list dynamically by parsing the asset pack config file.
+	while (true) {
+		String file = da->get_next();
+		if (file == "") {
+			break;
+		}
+		bool need_to_delete = true;
+		for (int i = 0; i < 14; i++) { //TODO: use a better data structure for faster lookup
+			if (file == keep[i]) {
+				need_to_delete = false;
+			}
+		}
+		if (need_to_delete) {
+			print_line("need to delete"); //TODO: delete unnecessary debugging print statements
+			if (file.find(".") == -1) {
+				DirAccessRef da = DirAccess::open("res://android/build/" + file);
+				da->erase_contents_recursive();
+			}
+			da->remove(file);
+		}
+		print_line(file); //TODO: delete unnecessary debugging print statements
+	}
+	da->list_dir_end();
+	return OK;
+}
 #endif //GODOT_GRADLE_EXPORT_UTIL_H
